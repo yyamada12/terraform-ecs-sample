141bf6cc6b8f8861f550bc2cfc2ca3e2c7d1a8c7
yyamada12 <12yacropolisy@gmail.com>
2023-06-21 22:51:33 +0900

    format main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region  = "ap-northeast-1"
-#   .envrc で設定
-#   export AWS_ACCESS_KEY_ID="your_access_key"
-#   export AWS_SECRET_ACCESS_KEY="your_secret_key"
-# 
-#   or ベタがき
-#   access_key = "your_access_key"
-#   secret_key = "your_secret_key"
+  region = "ap-northeast-1"
+  #   .envrc で設定
+  #   export AWS_ACCESS_KEY_ID="your_access_key"
+  #   export AWS_SECRET_ACCESS_KEY="your_secret_key"
+  # 
+  #   or ベタがき
+  #   access_key = "your_access_key"
+  #   secret_key = "your_secret_key"
 }
 
 ####################################################
@@ -22,9 +22,9 @@ resource "aws_ecs_cluster" "nginx_cluster" {
 ####################################################
 
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "ecs_task_execution_role"
+  name = "ecs_task_execution_role"
   assume_role_policy = jsonencode({
-    Version   = "2012-10-17"
+    Version = "2012-10-17"
     Statement = [
       {
         Effect    = "Allow"
