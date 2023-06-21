dac4b265ad6747a646cdcb8fda9e16bdcdfe689f
yyamada12 <12yacropolisy@gmail.com>
2023-06-21 22:31:08 +0900

    add IAM Role for ECS

--- main.tf
+++ main.tf
@@ -9,6 +9,31 @@ provider "aws" {
 #   secret_key = "your_secret_key"
 }
 
+####################################################
+# ECS Cluster
+####################################################
 resource "aws_ecs_cluster" "nginx_cluster" {
   name = "nginx-cluster"
+}
+
+
+####################################################
+# ECS IAM Role
+####################################################
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs_task_execution_role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "ecs-tasks.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
 }
